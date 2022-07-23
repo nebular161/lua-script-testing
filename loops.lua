c76f9865ee9516382b3f6ddbe30e533105acfb02
DarkMoon <40523372+DarkMoonPlayz1@users.noreply.github.com>
2022-07-23 12:23:32 -0400

    New code examples
    Final

---
+++ loops.lua
@@ -0,0 +1,28 @@
+counter = 1
+
+while counter <= 5 do -- Example of a loop
+
+    print(counter)
+    counter = counter + 1
+
+end
+
+-------------------------------------------
+--[[counter = 1
+
+repeat
+
+    print(counter)
+    counter = counter + 1
+
+until counter == 10
+
+while false do
+    counter = counter + 1
+    print(counter)
+end
+
+repeat
+    counter = counter + 1
+    print(counter)
+until true]]--
